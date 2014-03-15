d0ae4ff5eeff46e6163217c7609958b306718080
Jeremy Leng <jleng@bertramcapital.com>
2014-03-15 15:47:10 -0700

    removed plugins

--- plugins/time-series-0.1-SNAPSHOT/grails-app/conf/TimeSeriesBootStrap.groovy
+++
@@ -1,9 +0,0 @@
-class TimeSeriesBootStrap {
-	def grailsApplication
-	def init = { servletContext ->
-		grailsApplication.mainContext['timeSeriesService'].init()
-	}
-	def destroy = {
-		grailsApplication.mainContext['timeSeriesService'].destroy()
-	}
-}
